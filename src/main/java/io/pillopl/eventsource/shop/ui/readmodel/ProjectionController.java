72ffcf4dd542692a53548068cdb9e2c08b14e404
pilo <jakub.pilimon@4finance.com>
2016-10-05 17:06:30 +0200

    orderBy

--- src/main/java/io/pillopl/eventsource/shop/ui/readmodel/ProjectionController.java
+++ src/main/java/io/pillopl/eventsource/shop/ui/readmodel/ProjectionController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 @RestController
 public class ProjectionController {
 
-    private static final String SELECT_WHOLE_PROJECTION = "select uuid, status, when_ordered, when_paid, when_payment_timeout, when_payment_marked_as_missing from items";
+    private static final String SELECT_WHOLE_PROJECTION = "select uuid, status, when_ordered, when_paid, when_payment_timeout, when_payment_marked_as_missing from items order by when_ordered";
     private final JdbcTemplate jdbcTemplate;
 
     @Autowired
