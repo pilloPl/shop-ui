d7eabe4a56862ec4145ede4c8aeb4777fcf6f2ee
pilo <jakub.pilimon@4finance.com>
2016-10-05 17:12:31 +0200

    price is visible

--- src/main/java/io/pillopl/eventsource/shop/ui/readmodel/ShopItem.java
+++ src/main/java/io/pillopl/eventsource/shop/ui/readmodel/ShopItem.java
@@ -2,6 +2,7 @@ package io.pillopl.eventsource.shop.ui.readmodel;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 @Data
@@ -10,6 +11,7 @@ public class ShopItem {
     private Long id;
     private String uuid;
     private String status;
+    private BigDecimal amount;
     private Timestamp when_ordered;
     private Timestamp when_paid;
     private Timestamp when_payment_timeout;
